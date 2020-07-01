8bb3f46e5ea5bff6427f3740f8980e37a00f7da9
Yury Yurchenko <yuzbaster@gmail.com>
2020-07-01 18:52:31 +0300

    dev update

--- dsp_cam.py
+++ dsp_cam.py
@@ -5,24 +5,26 @@ from rpi_car_manager import *
 import time
 
 
+def cam_detect():
+   pass
 
 
-
-def send2manager(client, comp):
+def send2manager(client, msg2snt):
    # mqtt client
    tnow = time.localtime(time.time())
-   msg = 'Watcher Msg: ' + str(comp) + ' at ' + time.asctime(tnow)
+   msg = 'DSP_CAM Msg: ' + str(msg_device[3]) + ' at ' + time.asctime(tnow)
 
-   client.publish(sub_topic[1], msg)
+   client.publish(sub_topic[3], msg)
    print('message ' + msg + ' sent')
    pass
 
 if __name__ == "__main__":
-   cname = "RPI3_watcher-"
+   cname = "Car_dsp_cam-"
    client = client_init(cname)
    try:
       while conn_time == 0:
-         
+         if cam_detect():
+            send2manager(client,msg_device[3])
          print('send2manager procedure ended peacefully, next loop started')
    except KeyboardInterrupt:
       client.disconnect()  # disconnect from broker
